238b9a136e82fcf768a668f20502819e8ca3678f
Roman & Anastasiya <roma.gostev2017@yandex.ru>
2023-07-09 20:23:23 +0700

    task15

---
+++ task15/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите цифру соответсвующую дню недели: ");
+string hollyDay = Console.ReadLine();
+if ((hollyDay.Equals("6")||hollyDay.Equals("7")))
+{
+    Console.WriteLine("Данный день является выходным днем");
+}
+else 
+{
+    Console.WriteLine("Данный день не является выходным днем");
+}
