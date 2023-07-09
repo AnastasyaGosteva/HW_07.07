1e16d048633cb555269cf7aaf3b9bd96a3f28e7d
Roman & Anastasiya <roma.gostev2017@yandex.ru>
2023-07-09 20:28:14 +0700

    task13

---
+++ task13/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Введите любое число, number: ");
+string number = Console.ReadLine();
+if (number.Length <3)
+{
+    Console.Write($"В числе {number} третей цифры нет.");
+}
+else
+{
+Console.Write($"Третья цифра числа {number} равна "+number[2]+".");   
+}
+
+
+// Console.Write("Введите любое число, number: ");
+// int number = int.Parse(Console.ReadLine());
+// string number1 = number.ToString();
+// int [] array = new int [number1.Length];
+// if (array.Length <3)
+// {
+//     Console.Write($"В числе {number} третей цифры нет.");
+// }
+// else
+// {
+// for (int i = 0; i < array.Length; i++)
+// {
+
+//     array[i] = int.Parse(number1[i].ToString());
+// }
+// Console.Write($"Третья цифра числа {number} равна "+array[2]+".");   
+// }
