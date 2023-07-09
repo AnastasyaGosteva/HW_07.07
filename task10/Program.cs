470eadf55c65254e43a1bc6669fc1a9749e2ff37
Roman & Anastasiya <roma.gostev2017@yandex.ru>
2023-07-09 20:23:07 +0700

    task10

---
+++ task10/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите трехзначное число: number = ");
+int number = int.Parse(Console.ReadLine());
+int secondDigit = ((number % 100)/10);
+Console.Write($"Вторая цифра числа {number} равна "+secondDigit);
